e7ff8820aa3c3ee47208998b7c58aec61f89c1a8
Daniel <karczmarczykdaniel@gmail.com>
2022-03-26 16:45:07 +0100

    fetching image, searching, debounce, dropmenu+ refresh with movie images

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-const input1 = document.querySelector('#input1');
+const root = document.querySelector('.autocomplete');
+root.innerHTML = ` 
+	<label><b>search for movie</b></label>
+	<input class="input" />
+	<div class="dropdown">
+		<div class="dropdown-menu"> 
+			<div class="dropdown-content results"> 
+			</div>
+		</div>
+	</div>
+`;
+const input1 = document.querySelector('.input');
 
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 const debounce = (func) => {
 	let timeoutID;
 	return (...args) => {
@@ -19,11 +32,24 @@ const fetchData = async (input) => {
 			s: input
 		}
 	});
-	return console.log(response.data.Search);
+	if (response.data.Error) return [];
+	return response.data.Search;
 };
 
-const onInput = (event) => {
-	fetchData(event.target.value);
+const onInput = async (event) => {
+	const movies = await fetchData(event.target.value);
+	dropdown.classList.add('is-active');
+	//console.log(movies);
+	resultsWrapper.innerHTML = '';
+	for (let movie of movies) {
+		const div = document.createElement('div');
+		const moviePoster = movie.Poster === 'N/A' ? '' : movie.Poster;
+		div.innerHTML = `
+		<img src="${moviePoster}" />
+		<h1>${movie.Title}</h1>
+		`;
+		resultsWrapper.appendChild(div);
+	}
 };
 
 input1.addEventListener('input', debounce(onInput));
