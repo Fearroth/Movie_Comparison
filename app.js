ce5e70564fd840c8288e766144b7334f98af57fd
Daniel <karczmarczykdaniel@gmail.com>
2022-03-30 18:35:45 +0200

    parsing data, value added, comparison

--- app.js
+++ app.js
@@ -40,6 +40,22 @@ createAutoComplete({
 });
 
 const movieTemplate = (movieDetail) => {
+	const dollars = movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, '');
+	const metascore = parseInt(movieDetail.Metascore);
+	const imdbRating = parseFloat(movieDetail.imdbRating);
+	const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''));
+
+	const awards = movieDetail.Awards.split(' ').reduce((prev, word) => {
+		const value = parseInt(word);
+		if (isNaN(value)) {
+			return prev;
+		} else {
+			return prev + value;
+		}
+	}, 0);
+
+	console.log(awards);
+
 	return `
 	  <article class="media">
 		<figure class="media-left">
@@ -55,23 +71,23 @@ const movieTemplate = (movieDetail) => {
 		  </div>
 		</div>
 	  </article>
-	  <article class="notification is-primary">
+	  <article data-value=${awards} class="notification is-primary">
 		<p class="title">${movieDetail.Awards}</p>
 		<p class="subtitle">Awards</p>
 	  </article>
-	  <article class="notification is-primary">
+	  <article data-value=${dollars} class="notification is-primary">
 		<p class="title">${movieDetail.BoxOffice}</p>
 		<p class="subtitle">Box Office</p>
 	  </article>
-	  <article class="notification is-primary">
+	  <article data-value=${metascore} class="notification is-primary">
 		<p class="title">${movieDetail.Metascore}</p>
 		<p class="subtitle">Metascore</p>
 	  </article>
-	  <article class="notification is-primary">
+	  <article data-value=${imdbRating} class="notification is-primary">
 		<p class="title">${movieDetail.imdbRating}</p>
 		<p class="subtitle">IMDB Rating</p>
 	  </article>
-	  <article class="notification is-primary">
+	  <article data-value=${imdbVotes} class="notification is-primary">
 		<p class="title">${movieDetail.imdbVotes}</p>
 		<p class="subtitle">IMDB Votes</p>
 	  </article>
@@ -100,5 +116,21 @@ const onMovieSelect = async (movie, summaryRoot, side) => {
 	}
 };
 const runComparison = () => {
-	console.log('test comparison');
+	const leftSideStats = document.querySelectorAll('#left-summary .notification');
+	const rightSideStats = document.querySelectorAll('#right-summary .notification');
+
+	leftSideStats.forEach((leftStat, index) => {
+		const rightStat = rightSideStats[index];
+
+		const leftSideValue = leftStat.dataset.value;
+		const rightSideValue = rightStat.dataset.value;
+
+		if (rightSideValue > leftSideValue) {
+			leftStat.classList.remove('is-primary');
+			leftStat.classList.add('is-warning');
+		} else {
+			rightStat.classList.remove('is-primary');
+			rightStat.classList.add('is-warning');
+		}
+	});
 };
